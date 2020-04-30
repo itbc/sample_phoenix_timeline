aee6c72c4033b219b21a0a2907cd206212567ed4
itbc <it@Tomos-MacBook-Pro.local>
2020-05-01 01:10:03 +0900

    first commit

--- lib/chirp/timeline/post.ex
+++ lib/chirp/timeline/post.ex
@@ -6,7 +6,7 @@ defmodule Chirp.Timeline.Post do
     field :body, :string
     field :likes_count, :integer, default: 0
     field :reposts_count, :integer, default: 0
-    field :username, :string, default: "torifukukaiou"
+    field :username, :string, default: "sample_Build_a_real-time_Twitter"
 
     timestamps()
   end
